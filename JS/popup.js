ee82a899e9f32964554e49cab31909311b025c32
bidiantoro <erfanbidiantoro@gmail.com>
2020-06-16 11:44:03 +0700

    update JS

---
+++ JS/popup.js
@@ -0,0 +1,13 @@
+alert ("selamat datang");
+var coba = true;
+
+while (coba) {
+    var nama = prompt ("Masukan Nama Anda : ");
+    alert("Halo " + nama);
+
+    coba = confirm ('Masukan Nama Lagi ?')
+
+
+}
+
+alert ("Terima kasih , Selamat Berkunjung");
