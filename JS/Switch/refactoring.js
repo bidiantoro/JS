8b618fce5d357e7b7c9c10103cf397a738212288
bidiantoro <erfanbidiantoro@gmail.com>
2020-06-16 23:50:59 +0700

    update refactoring dll di switch

---
+++ JS/Switch/refactoring.js
@@ -0,0 +1,17 @@
+function jumlahVolumeDuaKubus (a,b) {
+    return a * a * a + b * b * b
+// var volumeA;
+// var volumeB;
+// var total;
+
+// volumeA = a * a * a;
+// volumeB = b * b * b;
+
+// total= volumeA + volumeB
+
+// return total;
+
+}
+
+alert(jumlahVolumeDuaKubus(8,3));
+
